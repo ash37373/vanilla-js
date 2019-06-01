4ece0a3139117afd20b8752ea30254f6f46730c0
ash37373 <ash37373@gmail.com>
2019-06-02 00:46:49 +0900

    0602

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-// const title = document.getElementById("title");
-const title = document.querySelector("#title");
-// console.log(title);
-title.innerHTML = "Hi! FUCK";
-title.style.color = "red";
+// // const title = document.getElementById("title");
+// const title = document.querySelector("#title");
+// // console.log(title);
+// title.innerHTML = "Hi! FUCK";
+// title.style.color = "red";
 
-console.dir(title);  //이벤트들이 쭉 나옴.
-document.title = "no something";
-//git 재업테스트
+// console.dir(title);  //이벤트들이 쭉 나옴.
+// document.title = "no something";
+// //git 재업테스트
+
+
+
+// //0602
+// const title = document.querySelector("#title");
+
+// // function handleResize(){
+// //     console.log("I have been resized");
+// // }
+// // window.addEventListener("resize", handleResize());
+
+// // function handleResize(event){
+// //     console.log(event);
+// // }
+// // window.addEventListener("resize", handleResize);
+
+
+// function handleClick(){
+//     title.style.color = "blue";
+// }
+// title.addEventListener("click", handleClick);
+
+//if - else 설명
+// if(10 > 5){
+//     console.log("Hi");
+// }else{
+//     console.log("ho");
+// }
+
+// if(20 > 5 && "nicolas" === "nicolas"){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// true && true = true;
+// false && true = false;
+// true && false = false;
+// false && false = false;
+
+// const age = prompt("How old are you?");
+
+// if (age>=18 && age<=21){
+//     console.log('you can drink but you should not');
+// }else if(age>21){
+//     console.log('go ahed');
+// }else{
+//     console.log('too young');
+// }
+
+
+// #2-6 DOM If else Function practice 할 차례임.
